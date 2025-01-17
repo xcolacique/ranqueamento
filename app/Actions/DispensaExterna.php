e19841f5f63dedc91d6da7866ac0b7cd98df639f
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2025-01-17 18:14:59 -0300

    Update DispensaExterna.php

--- app/Actions/DispensaExterna.php
+++ app/Actions/DispensaExterna.php
@@ -28,11 +28,12 @@ class DispensaExterna
         $aproveitamentos = DB::fetchAll($query);
 
         return collect($aproveitamentos)->map(function ($aproveitamento) {
-            if(!is_numeric($aproveitamento['nota'])) $aproveitamento['nota'] = 0;
-            return [
-                'coddis' => $aproveitamento['coddis'],
-                'nota'   => $aproveitamento['nota'] / $aproveitamento['qtdedisc']
-            ];
+            if(is_numeric($aproveitamento['nota'])) {
+                return [
+                   'coddis' => $aproveitamento['coddis'],
+                   'nota'   => $aproveitamento['nota'] / $aproveitamento['qtdedisc']
+                ];  
+            }
         });
 
     }
