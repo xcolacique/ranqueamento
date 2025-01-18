171168d1e9d001d6fbf3a8e27840e37322eabd91
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2025-01-18 10:02:47 -0300

    item menu de exemplo removido

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -37,13 +37,6 @@ $right_menu = [
     [
         'key' => 'laravel-tools',
     ],
-    [
-        'text' => '<i class="fas fa-cog"></i>',
-        'title' => 'Configurações',
-        'target' => '_blank',
-        'url' => config('app.url') . '/item1',
-        'align' => 'right',
-    ],
 ];
 
 return [
