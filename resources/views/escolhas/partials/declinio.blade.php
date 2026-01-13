17504a68aed25504f440c7b24b2ab96b4a961717
Mônica Colacique <monica.colacique@gmail.com>
2026-01-13 19:16:10 -0300

    Retirando botao de reranqueamento e retirando inner join

--- resources/views/escolhas/partials/declinio.blade.php
+++ resources/views/escolhas/partials/declinio.blade.php
@@ -1,17 +1,19 @@
-@if(\App\Services\Utils::declinou())
-    <form class="form-inline" method="POST" action="{{ route('declinar') }}">
-        @csrf
-        <b>Declinou do português?</b>&nbsp;sim&nbsp;
-        <button type="submit"  role="form"class="btn btn-warning"  name="declinar" value=0 
-        onclick="return confirm('Tem certeza que deseja cancelar a declinação do português?');"> Cancelar declinação 
-        </button>
-    </form>
-@else 
-    <form class="form-inline" method="POST" action="{{ route('declinar') }}">
-        @csrf
-        <b>Declinou do português?</b>&nbsp;não&nbsp; 
-        <button type="submit" class="btn btn-warning" name="declinar" value=1 
-        onclick="return confirm('Tem certeza que deseja declinar do português?');"> Quero declinar 
-        </button>
-    </form>
+@if($ranqueamento && $ranqueamento->tipo != 'reranqueamento')
+    @if(\App\Services\Utils::declinou())
+        <form class="form-inline" method="POST" action="{{ route('declinar') }}">
+            @csrf
+            <b>Declinou do português?</b>&nbsp;sim&nbsp;
+            <button type="submit"  role="form"class="btn btn-warning"  name="declinar" value=0
+            onclick="return confirm('Tem certeza que deseja cancelar a declinação do português?');"> Cancelar declinação
+            </button>
+        </form>
+    @else
+        <form class="form-inline" method="POST" action="{{ route('declinar') }}">
+            @csrf
+            <b>Declinou do português?</b>&nbsp;não&nbsp;
+            <button type="submit" class="btn btn-warning" name="declinar" value=1
+            onclick="return confirm('Tem certeza que deseja declinar do português?');"> Quero declinar
+            </button>
+        </form>
+    @endif
 @endif
